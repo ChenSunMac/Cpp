71169c866090c6c266a9363e835f982ae2f0f37f
Stieve.Chen <chensunmac@gmail.com>
2017-05-06 23:30:17 -0400

    asp.net application

---
+++ Active_Service_Page/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Active_Service_Page.Startup))]
+namespace Active_Service_Page
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
