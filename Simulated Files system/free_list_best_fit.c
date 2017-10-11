f4daf28bac13fda5fef864e037ab36ed0ab27bc9
Chain <chensunmac@gmail.com>
2017-10-11 15:25:36 -0400

    a2

---
+++ Simulated Files system/free_list_best_fit.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdlib.h>
+
+#include "free_list.h"
+
+/* Implement the best fit algorithm to find free space for the
+   simulated file data.
+ */
+
+int get_free_block(FS *fs, int size) {
+        return -1;
+}
+
+
